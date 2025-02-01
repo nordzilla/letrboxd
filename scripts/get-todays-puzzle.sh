fa1902bffcc870d6950bb849273ae7e78c75a941
Erik Nordin <nordzillacode@gmail.com>
2025-02-01 09:29:27 -0600

    Add script to fetch today's puzzle input

---
+++ scripts/get-todays-puzzle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the root directory of the project
+cd "$(dirname $0)/../rust"
+
+# Build the project in release mode
+cargo build --release --package todays-puzzle
+
+cd ..
+
+# Check if the site/generated directory exists, and create it if it doesn't
+if [ ! -d "./site/generated" ]; then
+  mkdir -p "./site/generated"
+fi
+
+./rust/target/release/todays-puzzle "./site/generated/json" $1
